8761294935c1ded6fc449e4b034db4072e8199c4
Krysztal Huang <krysztal.huang@outlook.com>
2025-12-24 18:39:26 +0800

    Bump version to 1.21 support with minium Java21 required

--- publish.sh
+++ publish.sh
@@ -12,7 +12,6 @@ if [ ! -f "supported.json" ]; then
     exit 1
 fi
 
-# 使用jq提取版本列表并通过parallel并发执行
 jq -r '.[]' supported.json | \
 parallel -j 4 --halt now,fail=1 \
-    "echo '正在处理版本: {}'; SCALA3_VERSION={} ./gradlew publishMods"
+    "echo 'Processing version: {}'; SCALA3_VERSION={} ./gradlew build&&./gradlew publishMods"
